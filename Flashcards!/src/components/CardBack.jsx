c3ebd4155dfac2ffe30106b7707f78f9dbd71be5
aisha <aishabeltagy1@gmail.com>
2024-10-08 00:48:54 -0500

    project 03

--- Flashcards!/src/components/CardBack.jsx
+++ Flashcards!/src/components/CardBack.jsx
@@ -4,7 +4,7 @@ const CardBack = ({ answer, image }) => {
     return (
         <div className='cardback'>
             <h3> {answer}</h3>
-            <img src={image} alt="Visual representation" />
+            <img src={image} alt="Visual representation" style={{ width: "300px", height: "300px" }} />
             
         </div>
     );
